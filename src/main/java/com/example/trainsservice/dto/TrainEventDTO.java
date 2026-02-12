4d493c76b01007e4ab8e27c5b8bed1cf6e8671cc
Maru <pdijsj2992@gmail.com>
2026-02-12 16:21:12 +0300

    Revert "feat(messaging): implement Kafka producer and consumer for train events"
    This reverts commit cd0af04390e6cd4e35ade52e43d3588ed5fa0e77.

--- src/main/java/com/example/trainsservice/dto/TrainEventDTO.java
+++ src/main/java/com/example/trainsservice/dto/TrainEventDTO.java
@@ -1,32 +1,4 @@
 package com.example.trainsservice.dto;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public class TrainEventDTO {
-
-    private String trainId;
-    private String status;
-
-    public TrainEventDTO() {}
-
-    public String getTrainId(){
-        return getTrainId();
-    }
-
-    public void setTrainId(
-            String trainId
-    ){
-        this.trainId = trainId;
-    }
-
-    public String getStatus(){
-        return status;
-    }
-
-    public void setStatus(
-            String status
-    ){
-        this.status = status;
-    }
 }
--- src/main/java/com/example/trainsservice/service/messaging/TrainEventProducer.java
+++ src/main/java/com/example/trainsservice/service/messaging/TrainEventProducer.java
@@ -1,28 +1,4 @@
 package com.example.trainsservice.service.messaging;
 
-import com.example.trainsservice.dto.TrainEventDTO;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-@AllArgsConstructor
-@Slf4j
 public class TrainEventProducer {
-
-    private final KafkaTemplate<String, TrainEventDTO> kafkaTemplate;
-
-    public void sendEvent(
-            TrainEventDTO event
-    ){
-        kafkaTemplate.send("trains-events", event.getTrainId(), event)
-                .whenComplete((result, ex) -> {
-                    if(ex == null){
-                        System.out.println("SUCCESS: Message sent to topic " + result.getRecordMetadata().topic());
-                    }else{
-                        System.out.println("ERROR: Failed to send message due to: " + ex.getMessage());
-                    }
-                });
-    }
 }
